a3640dccccfc551257851ba3348c318ba7269c96
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-01-11 14:35:03 +0200

    refactor Canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,13 +1,21 @@
 import Color from "./math/Color.js"
+import RayTracer from "./RayTracer.js"
+import Viewport from "./Viewport.js"
 
 /**
 * @typedef {Object} CanvasOptions
 * @property {number} width  - Canvas width in pixels
 * @property {number} height - Canvas height in pixels
+* @property {Canvas.RayTraceDrawMode[keyof Canvas.RayTraceDrawMode]} rayTraceDrawMode
+* @property {Color} backroundColor - Canvas backround color
 */
 
 export default class Canvas {
-    #COLOR_BACKGROUND = '#000000'
+    static RayTraceDrawMode = Object.freeze({
+        FASTEST: 0,
+        SLOW: 1,
+        SLOWEST: 2
+    })
 
     /** @private @type{HTMLCanvasElement} */ #canvas
     /** @private @type{CanvasRenderingContext2D} */ #context
@@ -27,12 +35,30 @@ export default class Canvas {
         if (this.#canvas === null) throw new Error("Context not found")
 
         if (typeof options !== 'object') throw new TypeError("Parameter 'options' is not object")
+
+        this.backroundColor = options.backroundColor ?? new Color()
+        this.rayTraceDrawMode = options.rayTraceDrawMode ?? Canvas.RayTraceDrawMode.FASTEST
         this.width = options.width
         this.height = options.height
     }
 
-    get width() { return this.#options.width }
+    get backroundColor() { return this.#options.backroundColor }
+    set backroundColor(color) {
+        if (!(color instanceof Color)) throw new TypeError("Parameter 'color' is not Color")
 
+        this.#options.backroundColor = color
+    }
+
+    get rayTraceDrawMode() { return this.#options.rayTraceDrawMode }
+    set rayTraceDrawMode(mode) {
+        if (!Object.values(Canvas.RayTraceDrawMode).includes(mode)) {
+            throw new TypeError(`Parameter 'options.rayTraceDrawMode' option ${rayTraceDrawMode} does not exist`)
+        }
+
+        this.#options.rayTraceDrawMode = mode
+    }
+
+    get width() { return this.#options.width }
     /** @param {number} pixels - Must be positive */
     set width(pixels) {
         this.#validateDimension(pixels)
@@ -42,7 +68,6 @@ export default class Canvas {
     }
 
     get height() { return this.#options.height }
-
     /** @param {number} pixels - Must be positive */
     set height(pixels) {
         this.#validateDimension(pixels)
@@ -54,6 +79,32 @@ export default class Canvas {
     #validateDimension(pixels) {
         if (typeof pixels !== 'number') throw new TypeError("Parameter 'pixels' is not number")
         if (pixels <= 0) throw new RangeError("Parameter 'pixels' is negative or zero")
+    }
+
+    /**
+    * @param {Viewport} viewport
+    * @param {RayTracer} rayTracer
+    *
+    */
+    async rayTrace(viewport, rayTracer) {
+        this.clear()
+
+        for (let x = -this.width / 2; x < this.width / 2; x++) {
+            for (let y = -this.height / 2; y < this.height / 2; y++) {
+                const ray = viewport.fromCanvas(x, y, this)
+                const color = rayTracer.trace(ray)
+
+                this.putPixel(x, y, color ?? this.backroundColor)
+
+                if (this.rayTraceDrawMode === Canvas.RayTraceDrawMode.SLOWEST) {
+                    await new Promise(requestAnimationFrame)
+                }
+            }
+
+            if (this.rayTraceDrawMode === Canvas.RayTraceDrawMode.SLOW) {
+                await new Promise(requestAnimationFrame)
+            }
+        }
     }
 
     /**
@@ -76,7 +127,7 @@ export default class Canvas {
     }
 
     clear() {
-        this.#context.fillStyle = this.#COLOR_BACKGROUND
+        this.#context.fillStyle = this.backroundColor.hex
         this.#context.fillRect(0, 0, this.width, this.height)
     }
 }
--- src/index.js
+++ src/index.js
@@ -7,31 +7,19 @@ import Sphere from "./object/Sphere.js"
 import RayTracer from "./RayTracer.js"
 import Viewport from "./Viewport.js"
 
-const canvas = new Canvas('#canvas', { width: 500, height: 500 })
 const viewport = new Viewport({ width: 1, height: 1 }, 1)
 const camera = new Camera(new Vector3())
-
-const scene = new Scene()
-const rayTracer = new RayTracer(camera, scene, viewport.distanceToCamera, 100)
 
 const sphere1 = new Sphere(new Vector3(0, -1, 3), 1, new Color(255, 0, 0))
 const sphere2 = new Sphere(new Vector3(2, 0, 4), 1, new Color(0, 0, 255))
 const sphere3 = new Sphere(new Vector3(-2, 0, 4), 1, new Color(0, 255, 0))
+
+const scene = new Scene()
 scene.add(sphere1)
 scene.add(sphere2)
 scene.add(sphere3)
 
-canvas.clear()
+const rayTracer = new RayTracer(camera, scene, viewport.distanceToCamera, 100)
 
-async function render() {
-    for (let x = -canvas.width / 2; x < canvas.width / 2; x++) {
-        for (let y = -canvas.height / 2; y < canvas.height / 2; y++) {
-            const ray = viewport.fromCanvas(x, y, canvas)
-            const color = rayTracer.trace(ray)
-            canvas.putPixel(x, y, color ?? new Color(255, 255, 255))
-        }
-    await new Promise(requestAnimationFrame)
-    }
-}
-
-render()
+const canvas = new Canvas('#canvas', { width: 500, height: 500, backroundColor: new Color(255, 255, 255) })
+canvas.rayTrace(viewport, rayTracer)

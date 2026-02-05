7aa1f6010e80d66597b763943bef1707ccbb5724
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-02-05 22:37:18 +0200

    refactor Color

--- src/Color.js
+++ src/Color.js
@@ -1,40 +1,45 @@
-import { assertInstancesMapped, assertNumbersBetween, assertNumbersUndefined } from "./Assert.js";
 import Vector3 from "./math/Vector3.js";
 
-export default class Color extends Vector3 {
-    /**
-    * @param {number} r - Must be between 0 and 255.
-    * @param {number} g - Must be between 0 and 255.
-    * @param {number} b - Must be between 0 and 255.
-    */
-    constructor(r = 0, g = 0, b = 0) {
-        super(r, g, b)
+export default class Color {
+    /** @type {Uint8ClampedArray} */ #components = new Uint8ClampedArray(4)
 
-        this.r = r
-        this.g = g
-        this.b = b
+    /**
+    * @param {number} [r=0] Red component (0–255)
+    * @param {number} [g=0] Green component (0–255)
+    * @param {number} [b=0] Blue component (0–255)
+    * @param {number} [a=255] Alpha component (0–255)
+    */
+    constructor(r = 0, g = 0, b = 0, a = 255) {
+        this.#components[0] = r
+        this.#components[1] = g
+        this.#components[2] = b
+        this.#components[3] = a
     }
 
-    get r() { return super.x }
+    get r() { return this.#components[0] }
     /** @param {number} number - Must be between 0 and 255. */
-    set r(number) { assertNumbersBetween({ number }, 0, 255); super.x = number }
+    set r(number) { this.#components[0] = number }
 
-    get g() { return super.y }
+    get g() { return this.#components[1] }
     /** @param {number} number - Must be between 0 and 255. */
-    set g(number) { assertNumbersBetween({ number }, 0, 255); super.y = number }
+    set g(number) { this.#components[1] = number }
 
-    get b() { return super.z }
+    get b() { return this.#components[2] }
     /** @param {number} number - Must be between 0 and 255. */
-    set b(number) { assertNumbersBetween({ number }, 0, 255); super.z = number }
+    set b(number) { this.#components[2] = number }
+
+    get a() { return this.#components[3] }
+    /** @param {number} number - Must be between 0 and 255. */
+    set a(number) { this.#components[3] = number }
 
     get hex() {
         /** @type {(value: number) => string} */
         const toHex = value => Math.round(value).toString(16).padStart(2, "0")
 
-        return `#${toHex(this.r)}${toHex(this.g)}${toHex(this.b)}`
+        return `#${toHex(this.#components[0])}${toHex(this.#components[1])}${toHex(this.#components[2])}${toHex(this.#components[3])}`
     }
 
-    get rgba() { return [this.r, this.g, this.b, 255] }
+    get rgba() { return [ this.#components[0], this.#components[1], this.#components[2], this.#components[3] ] }
 
     /**
     * @param {{
@@ -44,25 +49,20 @@ export default class Color extends Vector3 {
     *   }} [object]
     */
     static fromJSON({ x, y, z } = {}) {
-        assertNumbersUndefined({ x, y, z })
-
         return new Color(x, y, z)
     }
 
-    /**
-    * @param {Array<number>} array
-    */
+    /** @param {Array<number>} array */
     static fromArray(array) {
-        return new Color(array[0], array[1], array[2])
+        if (array.length === 3 || array.length === 4) {
+            return new Color(array[0], array[1], array[2], array[3])
+        } else {
+            throw new Error(`Bad argument array length given: ${array.length}`)
+        }
     }
 
     /** @param {Vector3} vector */
     static fromVector3(vector) {
-        assertInstancesMapped({ vector })
-
-        /** @type {(value: number) => number} */
-        const clamp = value => value < 0 ? 0 : value > 255 ? 255 : value
-
-        return new Color(clamp(vector.x), clamp(vector.y), clamp(vector.z))
+        return new Color(vector.x, vector.y, vector.z)
     }
 }
